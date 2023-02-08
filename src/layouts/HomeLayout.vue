5e737a63a261156bc5a2cabd5bc5f4f57c69c76c
ThaiPt <vuthaind@gmail.com>
2023-02-08 18:58:36 +0700

    update file Homelayout.vue

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -18,7 +18,7 @@
   import RunText from '@/components/RunText.vue'
   import LogoBanner from '@/components/LogoBanner.vue'
   import TabPage from '@/components/TabPage.vue'
-  import FooTer from '@/components/Footer.vue'
+  import FooTer from '@/components/FooTer.vue'
 
   import { useCounterStore } from '@/stores/counter';
 
