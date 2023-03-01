14de7eebde2968a1508d2b0a18c037c730803719
ThaiPt <vuthaind@gmail.com>
2023-03-01 14:59:35 +0700

    yhui

--- src/components/LogoBanner.vue
+++ src/components/LogoBanner.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col md:flex-row   rounded min-h-[200px]   bg-white drop-shadow-md dark:bg-slate-900 px-2 py-5 gap-2">
     <!-- Logo -->
-    <div v-for="admin in counter.admins" class="flex flex-col gap-3 rounded overflow-hidden items-center  justify-center  md:w-[250px] w-full">
-      <div class="w-[120px]" v-on:click="counter.opentab=1; counter.setStatevaoLocalStorage();"><router-link to="/" ><img alt="Image" :src="admin.Avatar"/></router-link></div>
+    <router-link to="/" > v-for="admin in counter.admins" class="flex flex-col gap-3 rounded overflow-hidden items-center  justify-center  md:w-[250px] w-full">
+      <div class="w-[120px]" v-on:click="counter.opentab=1; counter.setStatevaoLocalStorage();"><img alt="Image" :src="admin.Avatar"/></div>
       <div :class="'text-[20px] font-bold antialiased text-'+admin.Main_Color"><h1>{{admin.Brand_Name}}</h1></div>
-    </div>
+    </router-link>
     <!-- Banner -->
     <div class="flex flex-col items-center rounded   text-lime-900 dark:text-lime-700   w-full h-full px-4 lg:px-1">
       <div><h1 v-if="counter.language==1" class="text-2xl font-bold antialiased ">Thông báo</h1><h1 v-if="counter.language==2" class="text-2xl font-bold antialiased ">Notify</h1></div>
